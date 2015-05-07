e3a0e696292c428af56683d7eded556508600971
sayor20 <sayor2010@gmail.com>
2015-05-06 23:11:10 -0700

    modified:   May-06.c

--- May-06.c
+++ May-06.c
@@ -43,3 +43,39 @@ public class demo{
 		System.out.println(reverse);
 	}
 }
+
+//program to replace all spaces in a string with '%20'
+
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		String input="s athi sh";
+		space(input);		
+	}
+	
+	private static void space(String test) {
+		int count = 0;
+		for(int i=0;i<test.length();i++){
+			if(test.charAt(i)==' ')
+				++count;
+		}
+		int size=test.length()+(count*2);
+		char[] testchar=new char[size];
+		int k=0;
+		for(int j=0;j<test.length();j++,k++){
+			if(test.charAt(j)==' '){
+				testchar[k]='%';
+				testchar[++k]='2';
+				testchar[++k]='0';
+				}
+			else{
+			testchar[k]=test.charAt(j);
+			}
+		}
+		System.out.print(testchar);
+		
+	}
+}
+
